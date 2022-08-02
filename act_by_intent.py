c32bcfa1c361ac6d3a5e83be56ff97a8d8b0ec07
Aishikboy109 <ujanaishik109@gmail.com>
2022-08-02 18:15:27 +0530

    cleaned up some junk

--- act_by_intent.py
+++ act_by_intent.py
@@ -1,33 +1,32 @@
 import os
+from sys import platform
 import pyjokes
 import datetime
 
 def act_by_intent(query):
-    # print("ENTERED ACT BY INTENT METHOD!!!")
-    # print("hullo")
-    # if "search" or "what is" in query:
-    #     search(inp)
-
-    # if "weather" in query:
-    #     response = query_wit(inp)
-    #     location = response["entities"]["wit$location:location"][0]["resolved"]["values"][0]["name"]
-    #     weather(location)
-    #     return ""
+    if platform == "linux" or platform == "linux2":
+        OS = "linux"
+    elif platform == "darwin":
+        OS = "mac"
+    elif platform == "win32":
+        OS = "windows"
+    else:
+        OS = "unknown"
 
     if ("restart" and "computer") in query:
         res = "Restarting computer..."
-        # if OS == "linux":
-        #     os.system("reboot")
-        # else:
-        os.system("shutdown /r")
+        if OS == "linux":
+            os.system("reboot")
+        else:
+            os.system("shutdown /r")
         return res
 
     elif ("shutdown" or "shut the computer" or "poweroff the computer") in query:
         res = "Shutting the computer down..."
-        # if OS == "linux":
-        #     os.system("shutdown -h now")
-        # else:
-        os.system("shutdown /s")
+        if OS == "linux":
+            os.system("shutdown -h now")
+        else:
+            os.system("shutdown /s")
         return res
 
     elif ("joke" or "jokes" or "make me laugh") in query:
